7fc9ad0346263f32268f7346faaacbadb74ebf10
Kartik singh <kartiksinghks2005@gmail.com>
2026-03-11 18:52:03 +0530

    Delete skills.py

--- skills.py
+++
@@ -1,9 +0,0 @@
-skills_list = [
-    "python","machine learning","deep learning","nlp",
-    "sql","excel","tableau","power bi",
-    "data analysis","data science",
-    "tensorflow","pytorch","scikit-learn",
-    "streamlit","flask","django",
-    "statistics","data visualization",
-    "git","github","html","css","javascript"
-]
